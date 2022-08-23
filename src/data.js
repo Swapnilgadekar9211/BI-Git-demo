7707169191df6cb196b3dbfc512d47d532253e8f
swapnilgadekar9211 <swapnilgadekar9211@gmail.com>
2022-08-23 11:38:06 +0530

    shopping cart added

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const list = [
+    {
+        id:1,
+        title:"Cotton Shirt",
+        author:"Men's Regular Fit Round Neck Half Sleeved T-Shirt",
+        price:250,
+        img:"https://cdn.pixabay.com/photo/2017/01/13/04/28/blank-1976317_960_720.png",
+        ammount:1,
+    },
+    {
+        id:2,
+        title:"White Tshirt",
+        author:"Men's Regular Fit Round Neck Half Sleeved T-Shirt",
+        price:550,
+        img:"https://cdn.pixabay.com/photo/2017/01/13/04/56/t-shirt-1976334_960_720.png",
+        ammount:1,
+    },
+    {
+        id:3,
+        title:"Full Sleeved Tshirt",
+        author:"Men's Regular Fit Round Neck Half Sleeved T-Shirt",
+        price:450,
+        img:"https://cdn.pixabay.com/photo/2016/12/06/09/31/blank-1886008__340.png",
+        ammount:1,
+    }
+]
+export default list;
